37c03ce746065c83ea77e0466e0a0f9c6aa30c82
ktnechyba <kn9558a@american.edu>
2023-09-11 22:57:55 -0400

    creating new layout

--- Assignments/02_basics/Assets/Prefabs/playerMovement.cs
+++ Assignments/02_basics/Assets/Prefabs/playerMovement.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 3;
+
+    public float speed = 5.0f;
+    public float jumpForce = 5.0f;
+    public bool isOnGround = true;
+    private float horizontalInput;
+    private float forwardInput;
+    private Rigidbody playerRb;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(dir * _speed * Time.deltaTime);
+        //player forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        //player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
--- Assignments/02_basics/Assets/createLayout.cs
+++ Assignments/02_basics/Assets/createLayout.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class createLayout : MonoBehaviour
 {
+    public GameObject logsPrefab;
+    public GameObject collaredPeccaryPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = Instantiate(collaredPeccaryPrefab, new Vector3(-4.647161f, 0, 13.46f), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //float x = Random.Range(4, 2);
+        //float y = 2.3f;
+        //float z = 6.2;
+        //Vector3 pos = new Vector3(x, y, z);
+        //GameObject logs = Instantiate(logsPrefab, pos, Quaternion.identity);
     }
 }
